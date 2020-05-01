7312fde79cdd61dcd8f0be301730e30dc7771590
andrearufo <info@andrearufo.it>
2020-05-01 17:53:49 +0200

    Gestione

---
+++ functions.php
@@ -0,0 +1,178 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$space = 'tptq2jlkf1p1';
+$token = 'cCS4byrNsP8zuQ_NoMXK4RsSRV_l_n3nLhye3C2ux3U';
+
+$client = new \Contentful\Delivery\Client($token, $space);
+
+$query = new \Contentful\Delivery\Query();
+$query->setContentType('chitarra')->orderBy('fields.anno');
+
+$entries = $client->getEntries($query);
+// // echo json_encode($entries[0], 1);
+// echo '<pre>'.print_r($entries[0]->getSystemProperties()->getCreatedAt(), 1).'</pre>';
+// exit;
+$chitarre = [];
+
+foreach ($entries as $item) {
+    $chitarre[] = new Chitarra($item);
+}
+
+class Chitarra
+{
+    function __construct(Object $info)
+    {
+        $this->id = $info->getId();
+        $this->create = $info->getSystemProperties()->getCreatedAt();
+        $this->update = $info->getSystemProperties()->getUpdatedAt();
+        $this->marchio = $info->marchio;
+        $this->alias = $info->alias;
+        $this->modello = $info->modello;
+        $this->seriale = $info->seriale;
+        $this->voto = $info->voto;
+        $this->anno = $info->anno;
+        $this->circa = $info->circa;
+        $this->acquisita = $info->acquisita;
+        $this->produzione = $info->produzione;
+        $this->ponte = $info->ponte;
+        $this->configurazione = $info->configurazione;
+        $this->pickups = $info->pickups;
+        $this->tasti = $info->tasti;
+        $this->corpo = $info->corpo;
+        $this->top = $info->top;
+        $this->manico = $info->manico;
+        $this->tastiera = $info->tastiera;
+        $this->corde = $info->corde;
+        $this->costo = $info->costo;
+        $this->colore = $info->colore;
+        $this->scalaCorde = $info->scalaCorde;
+        $this->accordatura = $info->accordatura;
+        $this->descrizione = $info->descrizione;
+        $this->modifiche = $info->modifiche;
+        $this->fronte = $info->fronte;
+        $this->retro = $info->retro;
+        $this->galleria = $info->galleria;
+    }
+
+    public function create($format = 'd/m/Y'){
+        $time = $this->create->getTimestamp();
+        return date($format, $time);
+    }
+
+    public function update($format = 'd/m/Y'){
+        $time = $this->update->getTimestamp();
+        return date($format, $time);
+    }
+
+    public function title(){
+        return $this->marchio.' `'.$this->alias.'` '.$this->modello;
+    }
+
+    public function anno(){
+        if(!$this->anno){
+            return $this->void();
+        }elseif ($this->circa){
+            return $this->anno.' ca.';
+        }else{
+            return $this->anno;
+        }
+    }
+
+    public function seriale(){
+        return $this->seriale ?: $this->void();
+    }
+
+    public function corpo(){
+        return $this->corpo ?: $this->void();
+    }
+
+    public function top(){
+        return $this->top ?: $this->void();
+    }
+
+    public function manico(){
+        return $this->manico ?: $this->void();
+    }
+
+    public function tastiera(){
+        return $this->tastiera ?: $this->void();
+    }
+
+    public function tasti(){
+        return $this->tasti ?: $this->void();
+    }
+
+    public function colore(){
+        return $this->colore ?: $this->void();
+    }
+
+    public function ponte(){
+        return $this->ponte ?: $this->void();
+    }
+
+    public function configurazione(){
+        return $this->configurazione ?: $this->void();
+    }
+
+    public function pickups(){
+        return $this->pickups ?: $this->void();
+    }
+
+    public function corde(){
+        return $this->corde ?: $this->void();
+    }
+
+    public function scalaCorde(){
+        return $this->scalaCorde ?: $this->void();
+    }
+
+    public function accordatura(){
+        return $this->accordatura ?: $this->void();
+    }
+
+    public function costo(){
+        return $this->costo ?: $this->void();
+    }
+
+    public function voto(){
+        $max = 5;
+        $stars = [];
+
+        $icon_full = '<i class="fas fa-star"></i>';
+        $icon_empty = '<i class="far fa-star"></i>';
+
+        for ($i=0; $i < $this->voto; $i++) {
+            $stars[] = $icon_full;
+        }
+        for ($i=0; $i < $max-$this->voto; $i++) {
+            $stars[] = $icon_empty;
+        }
+
+        $stars = implode('', $stars);
+        return $stars;
+    }
+
+    public function produzione(){
+        return $this->produzione ?: $this->void();
+    }
+
+    public function descrizione(){
+        if(!$this->descrizione) return false;
+
+        $renderer = new Contentful\RichText\Renderer();
+        return $renderer->render($this->descrizione);
+    }
+
+    public function modifiche(){
+        if(!$this->modifiche) return false;
+
+        $renderer = new Contentful\RichText\Renderer();
+        return $renderer->render($this->modifiche);
+    }
+
+    private function void(){
+        return '<span class="void">n/a</span>';
+    }
+}
---
+++ index.php
@@ -0,0 +1,113 @@
+<?php include 'functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+
+    <meta charset="utf-8">
+    <title>Chitarre ~ Rufo</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="/dist/styles/main.css">
+
+</head>
+<body>
+
+    <section id="header">
+        <div class="container">
+
+            <h1>Le mie chitarre</h1>
+
+            <p class="lead">Negli anni ne ho collezionate un po'...</p>
+
+            <ol>
+                <?php foreach ($chitarre as $c): ?>
+                    <li>
+                        <a href="#<?php echo $c->id ?>">
+                            <?php echo $c->title() ?>
+                            (<?php echo $c->anno() ?>)
+                        </a>
+                    </li>
+                <?php endforeach ?>
+            </ol>
+
+            <hr>
+
+            <?php foreach ($chitarre as $c): ?>
+
+                <article class="py-5" id="<?php echo $c->id ?>">
+                    <div class="card">
+                        <div class="card-header">
+
+                            <h3>
+                                <?php echo $c->title() ?>
+                            </h3>
+
+                            <small>Ultimo aggiornamento <?php echo $c->update('d/m/Y H:i') ?></small>
+
+                        </div>
+                        <div class="card-body">
+
+                            <div class="row">
+                                <div class="col-lg-3">
+
+                                    <dl>
+                                        <dt>Anno</dt> <dd><?php echo $c->anno() ?></dd>
+                                        <dt>Acquisita</dt> <dd><?php echo $c->acquisita() ?></dd>
+                                        <dt>Seriale</dt> <dd><?php echo $c->seriale() ?></dd>
+                                        <dt>Voto</dt> <dd><?php echo $c->voto() ?></dd>
+                                        <dt>Produzione</dt> <dd><?php echo $c->produzione() ?></dd>
+                                        <dt>Costo</dt> <dd><?php echo $c->costo() ?></dd>
+                                        <dt>Scala Corde</dt> <dd><?php echo $c->scalaCorde() ?></dd>
+                                        <dt>Accordatura</dt> <dd><?php echo $c->accordatura() ?></dd>
+                                    </dl>
+                                </div>
+                                <div class="col-lg-3">
+
+                                    <dl>
+                                        <dt>Corpo</dt> <dd><?php echo $c->corpo() ?></dd>
+                                        <dt>Top</dt> <dd><?php echo $c->top() ?></dd>
+                                        <dt>Manico</dt> <dd><?php echo $c->manico() ?></dd>
+                                        <dt>Tastiera</dt> <dd><?php echo $c->tastiera() ?></dd>
+                                        <dt>Numero tasti</dt> <dd><?php echo $c->tasti() ?></dd>
+                                        <dt>Colore</dt> <dd><?php echo $c->colore() ?></dd>
+                                        <dt>Tipo di ponte</dt> <dd><?php echo $c->ponte() ?></dd>
+                                        <dt>Configurazione pickup</dt> <dd><?php echo $c->configurazione() ?></dd>
+                                        <dt>Tipo di pickups</dt> <dd><?php echo $c->pickups() ?></dd>
+                                        <dt>Corde</dt> <dd><?php echo $c->corde() ?></dd>
+                                    </dl>
+
+                                </div>
+                                <div class="col-lg">
+
+                                    <dl>
+                                        <?php if ($c->descrizione()): ?>
+                                            <dt>Descrizione</dt>
+                                            <dd><?php echo $c->descrizione() ?></dd>
+                                        <?php endif; ?>
+
+                                        <?php if ($c->modifiche()): ?>
+                                            <dt>Modifiche</dt>
+                                            <dd><?php echo $c->modifiche() ?></dd>
+                                        <?php endif; ?>
+                                    </dl>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </article>
+
+            <?php endforeach; ?>
+
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script src="/dist/scripts/main.js" charset="utf-8"></script>
+
+</body>
+</html>
