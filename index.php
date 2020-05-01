67c2b07fec3c1c692a0d356a80d9ea0b72659fdb
andrearufo <info@andrearufo.it>
2020-05-01 18:14:24 +0200

    Fix mobile

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php include 'functions.php'; ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<!doctype html>
+<html lang="en">
 <head>
-
+    
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <title>Chitarre ~ Rufo</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
